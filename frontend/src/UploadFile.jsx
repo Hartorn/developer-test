4ffcc8a54aaf73c162ad3e9f397b09308f2ba30b
Hartorn <hartorn.github@gmail.com>
2023-08-03 23:30:18 +0200

    feat(frontend): Add working page for frontend

---
+++ frontend/src/UploadFile.jsx
@@ -0,0 +1,46 @@
+import { useState, useRef } from 'react';
+
+function UploadFile({ onUploadSuccess, onUploadError, targetUrl }) {
+    const [file, setFile] = useState();
+    const inputFileRef = useRef(null);
+
+    const handleFileChange = (e) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    };
+
+    const handleUploadClick = () => {
+        if (!file) {
+            return;
+        }
+
+        const body = new FormData()
+        body.append('scenario_file', file)
+
+        fetch(targetUrl, {
+            method: 'POST',
+            body: body,
+        })
+            .then(res => res.status == 200 ? res.json() : res.text().then((err) => Promise.reject(err)))
+            .then(onUploadSuccess)
+            .catch(onUploadError);
+    };
+
+    return (
+        <div>
+            <div>
+                <button onClick={() => inputFileRef.current.click()}>Choose a file</button>
+                <input hidden type="file" onChange={handleFileChange} ref={inputFileRef} />
+            </div>
+            <div>
+                {file && <p>Chosen file: {file && `${file.name}`}</p>}
+            </div>
+            <div>
+                {file && <button onClick={handleUploadClick}>Upload</button>}
+            </div>
+        </div>
+    );
+}
+
+export default UploadFile;
